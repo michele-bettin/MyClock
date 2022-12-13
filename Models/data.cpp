478419d2a085668e9ca15ee20592f99d7c374345
michele-bettin <michele.bettin2002@gmail.com>
2022-12-13 12:51:07 +0100

    Aggiunto Data

--- Models/data.cpp
+++ Models/data.cpp
@@ -1,6 +1,26 @@
+
+
 #include "data.h"
+
+const char *NOMI_MESI[] =
+{
+    "Mese non valido",
+    "Gennaio",
+    "Febbraio",
+    "Marzo",
+    "Aprile",
+    "Maggio",
+    "Giugno",
+    "Luglio",
+    "Agosto",
+    "Settembre",
+    "Ottobre",
+    "Novembre",
+    "Dicembre"
+};
 
 Data::Data()
 {
 
 }
+
--- Models/data.h
+++ Models/data.h
@@ -4,6 +4,16 @@
 
 class Data
 {
+private:
+    unsigned int giorno;
+    unsigned int mese;
+    unsigned int anno;
+
+    const char *NOMI_MESI[];
+
+    const char *GIORNI_MESI[] { "0", "31", "28", "31", "30", "31", "30",
+            July, August, September, October, November, December };
+
 public:
     Data();
 };
